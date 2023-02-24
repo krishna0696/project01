6838e1d3ee9cdd15c073231e97285239211f2727
M V K S <61658599+krishna0696@users.noreply.github.com>
2023-02-24 16:56:09 +0530

    ff

--- main.tf
+++ main.tf
@@ -3,10 +3,6 @@ region ="ap-south-1"
   access_key="AKIAWWFY4564D5JCH3MY"
     secret_key="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"
   }
-resource "aws_instance" "one" {
-  ami="ami-0e742cca61fb65051"
-  instance_type="t2.micro"
-}
 resource "aws_s3_bucket" "two" {
   bucket = "mks"
 }
