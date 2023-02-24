93762ae85bbf7cd373681b437af259e4769af820
M V K S <61658599+krishna0696@users.noreply.github.com>
2023-02-24 17:12:30 +0530

    commit1

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ region ="ap-south-1"
   access_key="AKIAWWFY4564D5JCH3MY"
     secret_key="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"
   }
+resource "aws_instance" "one"{
+  ami= "ami-0e742cca61fb65051"
+  instance_type="t2.micro"
+  tags= {
+    Name= "krishn"
+  }
+}
 resource "aws_s3_bucket" "two" {
   bucket = "mks"
 }
