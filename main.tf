e17959904359c367e04f1c1886a84813eea52493
M V K S <61658599+krishna0696@users.noreply.github.com>
2023-02-24 15:34:50 +0530

    terra user commits

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+region ="ap-south-1"
+  access_key="AKIAWWFY4564D5JCH3MY"
+    secret_key="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"
+  }
+resource "aws_instance" "one" {
+  ami="ami-0e742cca61fb65051"
+  instance_type="t2.micro"
+  tags ={
+    Name="krishna"
+  }
+}
+resource "aws_s3_bucket" "two" {
+  bucket = "mk"
+}
+resource "aws_ebs_volume" "three"{
+  availability_zone = "ap-south-1"
+  size= 40
+  tags ={
+    Name=vks
+  }
+}
